d39d2f1712d57e8157935dd38f55ff0539f8b9c9
OMironov <Mironov.oleg94@gmail.com>
2015-12-13 17:52:36 +1000

    END

--- ContosoUniversity/ContosoUniversity/Controllers/DepartmentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/DepartmentController.cs
@@ -31,7 +31,11 @@ namespace ContosoUniversity.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Department department = await db.Departments.FindAsync(id);
+
+            // Create and execute raw SQL query.
+            string query = "SELECT * FROM Department WHERE DepartmentID = @p0";
+            Department department = await db.Departments.SqlQuery(query, id).SingleOrDefaultAsync();
+
             if (department == null)
             {
                 return HttpNotFound();
